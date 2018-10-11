c7720b507a0e0a18422b52bbc61e87dca640596d
Ben <benharvie94@gmail.com>
2018-10-11 13:51:57 +0100

    deckExists test passing

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.BitSet;
+
 public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<>();
+    }
+
+    public void populate(){
+        for (Suit suit : Suit.values()
+                ) {
+            for (Rank rank : Rank.values()
+                 ) {
+                Card card = new Card(rank, suit);
+                cards.add(card);
+            }
+        }
+    }
+
+    public ArrayList getCards() {
+        return cards;
+    }
 }
